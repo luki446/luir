3f15818adca826f7816bfea3a7eeb689c7ae2619
Luki446 <luki446@gmail.com>
2023-02-24 20:31:33 +0100

    Add unit test for expression evaluation

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub enum EvalValue {
     Number(f64),
 }
@@ -37,5 +38,56 @@ impl Expression for BinaryExpression {
             },
             _ => Err("Invalid operands".to_string()),
         }
+    }
+}
+
+#[cfg(test)]
+mod ast_tests {
+    use super::*;
+
+    #[test]
+    fn test_number_expression() {
+        let expr = NumberExpression { value: 5.0 };
+        assert_eq!(expr.execute().unwrap(), EvalValue::Number(5.0));
+    }
+
+    #[test]
+    fn test_binary_addition_on_2_numbers() {
+        let expr = BinaryExpression {
+            left: Box::new(NumberExpression { value: 5.0 }),
+            operator: "+".to_string(),
+            right: Box::new(NumberExpression { value: 5.0 }),
+        };
+        assert_eq!(expr.execute().unwrap(), EvalValue::Number(10.0));
+    }
+
+    #[test]
+    fn test_binary_subtraction_on_2_numbers() {
+        let expr = BinaryExpression {
+            left: Box::new(NumberExpression { value: 5.0 }),
+            operator: "-".to_string(),
+            right: Box::new(NumberExpression { value: 5.0 }),
+        };
+        assert_eq!(expr.execute().unwrap(), EvalValue::Number(0.0));
+    }
+
+    #[test]
+    fn test_binary_multiplication_on_2_numbers() {
+        let expr = BinaryExpression {
+            left: Box::new(NumberExpression { value: 5.0 }),
+            operator: "*".to_string(),
+            right: Box::new(NumberExpression { value: 5.0 }),
+        };
+        assert_eq!(expr.execute().unwrap(), EvalValue::Number(25.0));
+    }
+
+    #[test]
+    fn test_binary_division_on_2_numbers() {
+        let expr = BinaryExpression {
+            left: Box::new(NumberExpression { value: 5.0 }),
+            operator: "/".to_string(),
+            right: Box::new(NumberExpression { value: 5.0 }),
+        };
+        assert_eq!(expr.execute().unwrap(), EvalValue::Number(1.0));
     }
 }
